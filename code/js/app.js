da0984f708e9024cc4f897a5595c2ecbc4893fbb
ddialar <ddialar@gmail.com>
2016-04-16 19:49:38 +0000

    refactor(testing-angular-unit-spec): Testing controllers.

--- code/js/app.js
+++ code/js/app.js
@@ -6,4 +6,24 @@ app
 
 function testingAngularCtrl($rootScope, $scope) {
   $scope.title = 'Testing Angular with Jasmine';
+
+  $scope.destinations = [];
+
+  $scope.newDestination = {
+    'city': undefined,
+    'country': undefined
+  };
+
+  $scope.addDestination = function() {
+    $scope.destinations.push(
+      {
+        'city': $scope.newDestination.city,
+        'country': $scope.newDestination.country
+      }
+    );
+  };
+
+  $scope.removeDestination = function(index) {
+    $scope.destinations.splice(index, 1);
+  };
 };
--- code/js/tests/unit/testingAngularUnitSpec.js
+++ code/js/tests/unit/testingAngularUnitSpec.js
@@ -17,5 +17,49 @@ describe('Testing Angular JS test suite', function() {
       expect(scope.title).toBeDefined();
       expect(scope.title).toBe('Testing Angular with Jasmine');
     });
+
+    it('should add 2 destinations to the destinations list', function() {
+      expect(scope.destinations).toBeDefined();
+      expect(scope.destinations.length).toBe(0);
+
+      scope.newDestination.city = 'London';
+      scope.newDestination.country = 'England';
+
+      scope.addDestination();
+
+      scope.newDestination.city = 'Fankfurt';
+      scope.newDestination.country = 'Germany';
+
+      scope.addDestination();
+
+      expect(scope.destinations.length).toBe(2);
+      expect(scope.destinations[0].city).toBe('London');
+      expect(scope.destinations[0].country).toBe('England');
+      expect(scope.destinations[1].city).toBe('Fankfurt');
+      expect(scope.destinations[1].country).toBe('Germany');
+    });
+
+    it('should remove a destination from the destination list', function() {
+      scope.destinations = [
+        {
+          'city': 'Paris',
+          'country': 'France'
+        },
+        {
+          'city': 'Warsaw',
+          'country': 'Poland'
+        }
+      ];
+
+      expect(scope.destinations.length).toBe(2);
+
+      scope.removeDestination(0);
+
+      expect(scope.destinations.length).toBe(1);
+      expect(scope.destinations[0].city).toBe('Warsaw');
+      expect(scope.destinations[0].country).toBe('Poland');
+    });
+
+
   });
 });
